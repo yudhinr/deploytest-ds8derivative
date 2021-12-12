86e12b8bef5bb1edda31d1b69a17ee71ca07ecfb
yudhinr <81465303+yudhinr@users.noreply.github.com>
2021-12-12 21:26:05 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -40,9 +40,6 @@ def data_processing(df):
   with open('scaler.sav', 'rb') as file:
     scaler = pickle.load(file)
 
-  # data = request.json
-  # df = pd.DataFrame(data, index = [0])
-
   df = df.drop(['User ID'], axis = 1)
   df['Gender'] = df['Gender'].map({'Male':1, 'Female':0})
   df.iloc[:,1:3] = scaler.transform(df.iloc[:,1:3])
@@ -51,20 +48,8 @@ def data_processing(df):
   with open('model_knn.pkl', 'rb') as file:
     model_knn = pickle.load(file)
 
-  # with open('model_svm.pkl', 'rb') as file:
-  #   model_svm = pickle.load(file)
-
   prediction_knn = model_knn.predict(df)
-  # prediction_svm = model_svm.predict(df)
 
-  # data['Prediction KNN'] = prediction_knn[0]
-  # data['Prediction SVM'] = prediction_svm[0]
-  # data['Datetime'] = datetime.now().strftime('%Y-%m-%d')
-
-  # with open('data_collection.txt', 'a') as file:
-  #   file.write("%s\n" % data)
-
-  # return jsonify({'Status': 'Berhasil', 'Prediction KNN':str(prediction_knn[0]), 'Prediction SVM':str(prediction_svm[0])})
   return prediction_knn
 
 
